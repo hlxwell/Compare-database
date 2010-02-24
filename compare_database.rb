2bffae27c73e07c5988474f7dd64b47d27d664f7
Michael He <hlxwell@gmail.com>
2010-02-24 12:14:12 +0800

    compare default value

--- compare_database.rb
+++ compare_database.rb
@@ -27,7 +27,7 @@ end
 class OldDB < ActiveRecord::Base
   establish_connection(
     :adapter  => 'mysql',
-    :database => 'joblet_from_pg',
+    :database => 'joblet_production',
     :username => 'root',
     :password => '',
     :host     => 'localhost'
@@ -61,21 +61,63 @@ def puts_migration_code(table, columns)
   end
 end
 
+def check_column_type(table, columns)
+  columns.each do |col|
+    #TODO: currently it't not support limit and other options
+    new_col_type = ""
+    old_col_type = ""
+    eval "old_col_type = #{table}Old.columns_hash['#{col}'].type.to_s"
+    eval "new_col_type = #{table}New.columns_hash['#{col}'].type.to_s"
+
+    if new_col_type != old_col_type
+      pp "change_column :#{table.tableize}, :#{col}, :#{old_col_type}, :#{new_col_type}"
+    end
+  end
+end
+
+def check_column_default_value(table, columns)
+  columns.each do |col|
+    #TODO: currently it't not support limit and other options
+    new_default_value = ""
+    old_default_value = ""
+    
+    eval "old_default_value = #{table}Old.columns_hash['#{col}'].default.to_s"
+    eval "new_default_value = #{table}New.columns_hash['#{col}'].default.to_s"
+
+    if new_default_value != old_default_value
+      pp "change_column_default(:#{table.tableize}, :#{col}, '#{new_default_value}')"
+      # pp "change_column :#{table.tableize}, :#{col}, :#{old_col_type}, :#{new_col_type}"
+    end
+  end
+end
+
 new_table_with_columns.each do |k,v|
   added_columns = removed_columns = []
   added_columns = v - old_table_with_columns[k] if old_table_with_columns[k]
   removed_columns = old_table_with_columns[k] - v if old_table_with_columns[k]
-  
+
+  if old_table_with_columns[k]
+    old_table_columns = old_table_with_columns[k]
+    new_table_columns = v
+
+    ### get common columns
+    common_columns = new_table_columns - removed_columns
+    check_column_type(k, common_columns)
+    check_column_default_value(k, common_columns)    
+  end
+
   if !added_columns.blank? or !removed_columns.blank?
     puts "=== diff of #{k} ==="
     unless added_columns.blank?
       puts "+ add columns"
       puts_migration_code k, added_columns
     end
-    
+
     unless removed_columns.blank?
       puts "- removed columns"
       puts_migration_code k, removed_columns
     end
+
+    v - (removed_columns + added_columns)
   end
 end
